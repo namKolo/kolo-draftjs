02fa6a0a158efbff12eff66b846225a91cf93421
Nam Kolo <pokemon.namkolo@gmail.com>
2017-10-30 11:56:37 +0700

    Add DraftJS Editor to App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import 'draft-js/dist/Draft.css';
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Editor, EditorState } from 'draft-js';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = editorState => this.setState({ editorState });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <div className="mui--text-center mui--text-headline margin-bottom-small">
+          Editor built on top of DraftJS
+        </div>
+        <div className="mui-panel">
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            placeholder="Tell something you like"
+          />
+        </div>
       </div>
     );
   }
